0315c24027b3cfe4f8218c14520c408478e93f72
Theo Malavieille <theo.malavieille.1@ens.etsmtl.ca>
2026-05-13 19:10:19 -0400

    Ajout de la premiere fonction de lecture du fichier

--- PenduGame.py
+++ PenduGame.py
@@ -1,12 +1,20 @@
-#--- jeu du pendu ---
-pendu =1
-print(pendu)
+# Cette fonction lit le fichier texte pour créer la liste de mots
 
-#ajout de test feature
-print('testtt')
-
-test2=2
-
-#branchedetestttt
-branchetest=1
-
+def lire_fichier_mots(nom_du_fichier):
+    try:
+        # On ouvre le fichier en mode lecture avec l'encodage pour les accents
+        f = open(nom_du_fichier, "r", encoding="utf-8")
+        contenu = f.readlines()
+        f.close()
+        
+        liste_finale = []
+        for ligne in contenu:
+            mot = ligne.strip() # On retire les espaces et sauts de ligne
+            if mot != "":
+                liste_finale.append(mot)
+        return liste_finale
+    except:
+        # Quand utilisateur donne un mauvais nom, on affiche une erreur
+        print("Erreur : Impossible de lire le fichier", nom_du_fichier)
+        return []
+    
