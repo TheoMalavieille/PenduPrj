8cb58efd831984f9f90db2b270f80f8b2df1b19f
Theo Malavieille <theo.malavieille.1@ens.etsmtl.ca>
2026-05-13 19:24:30 -0400

    Ajout des fonctions de normalisation de lettre et d'affichage de mot

--- PenduGame.py
+++ PenduGame.py
@@ -18,3 +18,30 @@ def lire_fichier_mots(nom_du_fichier):
         print("Erreur : Impossible de lire le fichier", nom_du_fichier)
         return []
     
+
+# Fonction qui remplace les lettres accentuées par des lettres simples
+
+def normaliser_lettre(lettre):
+    accents = {
+        'é': 'e', 'è': 'e', 'ê': 'e', 'ë': 'e', 'à': 'a', 'â': 'a', 
+        'ä': 'a', 'î': 'i', 'ï': 'i', 'ô': 'o', 'ö': 'o', 'û': 'u', 
+        'ù': 'u', 'ü': 'u', 'ç': 'c'
+    }
+    lettre = lettre.lower()
+    if lettre in accents:
+        return accents[lettre]
+    return lettre
+
+
+
+# Fonction qui crée la chaîne de caractères avec les tirets bas pour lettre invisible
+
+def creer_affichage(mot_complet, lettres_deja_trouvees):
+    affichage = ""
+    for lettre in mot_complet:
+        # On compare la version simplifiée de la lettre
+        if normaliser_lettre(lettre) in lettres_deja_trouvees:
+            affichage = affichage + lettre + " "
+        else:
+            affichage = affichage + "_ "
+    return affichage.strip()
